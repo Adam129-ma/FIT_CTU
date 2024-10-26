fad348989a72c87c80f08fc1b532adbd12de4734
Adam129-ma <115939245+Adam129-ma@users.noreply.github.com>
2024-10-26 16:56:13 +0200

    Delete PA2 directory

--- PA2/cmake-build-velkauloha/CMakeFiles/3.28.1/CMakeRCCompiler.cmake
+++
@@ -1,6 +0,0 @@
-set(CMAKE_RC_COMPILER "C:/Users/adamm/Desktop/School/4.semester/PA2/Velkauloha/winlibs-i686-posix-dwarf-gcc-12.3.0-llvm-16.0.4-mingw-w64ucrt-11.0.0-r1/mingw32/bin/windres.exe")
-set(CMAKE_RC_COMPILER_ARG1 "")
-set(CMAKE_RC_COMPILER_LOADED 1)
-set(CMAKE_RC_SOURCE_FILE_EXTENSIONS rc;RC)
-set(CMAKE_RC_OUTPUT_EXTENSION .obj)
-set(CMAKE_RC_COMPILER_ENV_VAR "RC")
